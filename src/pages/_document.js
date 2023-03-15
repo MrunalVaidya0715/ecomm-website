e873086955963edae4e17d377da4d92cc5239a43
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-03-15 15:24:48 +0530

    Added Service Woker

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="manifest" href="/manifest.json" />
+        
 
         <meta name="theme-color" content="#90cdf4" />
       </Head>
